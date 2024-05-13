e5e0d5e81ce9f9ea4f61c916fecf0c94ff04d5fd
Trapti Kapkoti <63699608+trapti099@users.noreply.github.com>
2024-05-13 22:26:47 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@
 employee_data_img_path = "assets/image0.png"
 company_location_img_path = "assets/image1.png"
 additional_data_img_path = "assets/image3.png"
-profile_img_path = "assets/image4.jpg"
+profile_img_path = "assets/image.jpg"
 background_image_path = "assets/image8.png"
 heading_img_path = "assets/image10.png"
 candidate_profile_img_path = "assets/image12.png"
